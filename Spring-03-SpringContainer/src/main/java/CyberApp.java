e7c4306e3ce82d689de976f86fd93af8c692f962
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-01 17:33:44 +0000

    spring container

---
+++ Spring-03-SpringContainer/src/main/java/CyberApp.java
@@ -0,0 +1,17 @@
+import interfaces.Mentor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CyberApp {
+
+    public static void main(String[] args) {
+
+        //BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        //Mentor mentor = (Mentor) container.getBean("fullTimeMentor");
+        //Mentor mentor = (Mentor) container.getBean("partTimeMentor");
+        Mentor mentor = container.getBean("fullTimeMentor",Mentor.class);
+        mentor.createAccount();
+    }
+}
