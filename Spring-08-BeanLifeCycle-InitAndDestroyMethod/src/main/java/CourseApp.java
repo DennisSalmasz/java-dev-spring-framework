7cfdbb341cfc6ff2fbcff17cbaf898d8f6945ece
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-01 23:59:03 +0000

    bean lifecycle - init & destroy methods

---
+++ Spring-08-BeanLifeCycle-InitAndDestroyMethod/src/main/java/CourseApp.java
@@ -0,0 +1,18 @@
+import com.cyber.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CourseApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course = container.getBean("java",Course.class);
+
+        course.getTeachingHours();
+
+        ((ClassPathXmlApplicationContext) container).close();
+        //this is to destroy bean!
+    }
+}
