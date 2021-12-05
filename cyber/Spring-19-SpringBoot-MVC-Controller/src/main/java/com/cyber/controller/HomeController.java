264df78026a10209526276c0a3ea733dec558c89
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-05 19:10:00 +0000

    SpringBoot MVC - Controller

--- cyber/Spring-19-SpringBoot-MVC-Controller/src/main/java/com/cyber/controller/HomeController.java
+++ cyber/Spring-19-SpringBoot-MVC-Controller/src/main/java/com/cyber/controller/HomeController.java
@@ -1,10 +1,7 @@
 package com.cyber.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller //business logic happens here
 public class HomeController {
@@ -35,6 +32,38 @@ public class HomeController {
 
     @PostMapping("/spring")
     public String getMappingEx2(){
+        return "home";
+    }
+
+    @GetMapping("/home/{name}")
+    public String pathVariable(@PathVariable String name){
+        System.out.println("name is " + name);
+        return "home";
+    }
+
+    @GetMapping("/home/{name}/{email}")
+    public String pathVariable2(@PathVariable String name, @PathVariable String email){
+        System.out.println("name is " + name);
+        System.out.println("email is " + email);
+        return "home";
+    }
+
+    //http://localhost:8080/home/course?course=spring
+    @GetMapping("/home/course")
+    public String requestParamEx(@RequestParam String course){
+        System.out.println("course is " + course);
+        return "home";
+    }
+
+    @GetMapping(value = "/course")
+    public String requestParamEx2(@RequestParam (value = "name", required = false, defaultValue = "cyber") String name){
+        System.out.println("course is " + name);
+        return "home";
+    }
+
+    @GetMapping("home/course2")
+    public String requestParamEx3(@RequestParam (value = "course2", required = false, defaultValue = "cyber") String name){
+        System.out.println("course is " + name);
         return "home";
     }
 }
