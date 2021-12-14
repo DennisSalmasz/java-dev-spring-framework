f90f66587b40c2631bd508e09c8fb6780e3e4b96
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-14 00:55:34 +0000

    ORM - table creation

---
+++ Spring-22-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.student(id,email,first_name,last_name) VALUES(1,'Mike','Smith','mike@cybertek.com');
+INSERT INTO public.student(id,email,first_name,last_name) VALUES(2,'Adam','Shaun','adam@cybertek.com');
+INSERT INTO public.student(id,email,first_name,last_name) VALUES(3,'Tom','Hanks','tom@cybertek.com');
+INSERT INTO public.student(id,email,first_name,last_name) VALUES(4,'Brad','Kelly','mike@cybertek.com');
+
