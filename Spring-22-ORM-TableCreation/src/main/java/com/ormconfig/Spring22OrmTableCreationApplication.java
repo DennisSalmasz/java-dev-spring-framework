46371ee54dd56d6ba503b30bd348040cc85ff506
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-14 04:52:25 +0000

    ORM - table creation - JPARepository Interface & @PostConstruct

--- Spring-22-ORM-TableCreation/src/main/java/com/ormconfig/Spring22OrmTableCreationApplication.java
+++ Spring-22-ORM-TableCreation/src/main/java/com/ormconfig/Spring22OrmTableCreationApplication.java
@@ -1,13 +1,32 @@
 package com.ormconfig;
 
+import com.ormconfig.entity.Car;
+import com.ormconfig.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Spring22OrmTableCreationApplication {
 
+    @Autowired
+    CarRepository carRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Spring22OrmTableCreationApplication.class, args);
+    }
+
+    @PostConstruct
+    public void dataInit(){
+        Car c1 = new Car("BMW","M5");
+        Car c2 = new Car("Kia","Sorento");
+        Car c3 = new Car("Mercedes","S500");
+
+        carRepository.save(c1);
+        carRepository.save(c2);
+        carRepository.save(c3);
     }
 
 }
