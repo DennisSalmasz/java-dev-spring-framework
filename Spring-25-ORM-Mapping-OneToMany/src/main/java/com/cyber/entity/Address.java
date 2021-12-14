1f2a93544dfee8b788a1185d784cb0a3b03aae02
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-14 23:17:00 +0000

    ORM - Mapping - ManyToOne

---
+++ Spring-25-ORM-Mapping-OneToMany/src/main/java/com/cyber/entity/Address.java
@@ -0,0 +1,17 @@
+package com.cyber.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String zipCode;
+
+    @ManyToOne
+    private Person person;
+}
---
+++ Spring-25-ORM-Mapping-OneToMany/src/main/java/com/cyber/entity/Person.java
@@ -0,0 +1,19 @@
+package com.cyber.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    //one person has many address
+    @OneToMany(mappedBy = "person")
+    private List<Address> address;
+}
