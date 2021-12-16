95029f37381114ebf88f724d9d46200cbb209143
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-16 22:29:45 +0000

    ORM - Mapping - JPQL

---
+++ Spring-29-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cyber/entity/Employee.java
@@ -0,0 +1,43 @@
+package com.cyber.entity;
+
+import com.cyber.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "employees")
+@ToString
+public class Employee extends BaseEntity{
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    @ManyToOne
+    @JoinColumn(name = "department")
+    private Department department;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer salary;
+
+    @ManyToOne
+    private Region region;
+}
---
+++ Spring-29-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cyber/repository/RegionRepository.java
@@ -0,0 +1,13 @@
+package com.cyber.repository;
+
+import com.cyber.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region,Integer> {
+
+
+}
