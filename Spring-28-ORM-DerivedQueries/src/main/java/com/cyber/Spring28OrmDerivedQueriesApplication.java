3705d175e1d670aefd9612f4641b5b521ca9b69c
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-16 19:41:48 +0000

    ORM - Mapping - Derived queries

--- Spring-28-ORM-DerivedQueries/src/main/java/com/cyber/Spring28OrmDerivedQueriesApplication.java
+++ Spring-28-ORM-DerivedQueries/src/main/java/com/cyber/Spring28OrmDerivedQueriesApplication.java
@@ -1,6 +1,8 @@
 package com.cyber;
 
 import com.cyber.entity.Region;
+import com.cyber.repository.DepartmentRepository;
+import com.cyber.repository.EmployeeRepository;
 import com.cyber.repository.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +16,10 @@ public class Spring28OrmDerivedQueriesApplication {
 
     @Autowired
     RegionRepository regionRepository;
+    @Autowired
+    DepartmentRepository departmentRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Spring28OrmDerivedQueriesApplication.class, args);
@@ -28,6 +34,34 @@ public class Spring28OrmDerivedQueriesApplication {
         System.out.println("findByCountryContainingOrderByCountry: " + regionRepository.findByCountryContainingOrderByCountry("Asia"));
         System.out.println("findTop2ByCountry: " + regionRepository.findTop2ByCountry("Canada"));
         System.out.println("-----------------Region end----------------");
+    }
+
+    @PostConstruct
+    public void testDepartments(){
+        System.out.println("-----------------Department start----------------");
+        System.out.println("findByDepartment: " + departmentRepository.findByDepartment("Furniture"));
+        System.out.println("findByDivision: " + departmentRepository.findByDivision("Health"));
+        System.out.println("findByDivisionIs: " + departmentRepository.findByDivisionIs("Health"));
+        System.out.println("findByDivisionEquals: " + departmentRepository.findByDivisionEquals("Health"));
+        System.out.println("findByDivisionEndingWith: " + departmentRepository.findByDivisionEndingWith("ics"));
+        System.out.println("findDistinctTop3ByDivisionContains: " + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+        System.out.println("-----------------Department end----------------");
+    }
+
+    @PostConstruct
+    public void testEmployees(){
+        System.out.println("-----------------Employee start----------------");
+        System.out.println("findByEmail: " + employeeRepository.findByEmail(""));
+        System.out.println("findByFirstNameAndLastNameOrEmail: " + employeeRepository.findByFirstNameAndLastNameOrEmail("","",""));
+        System.out.println("findByFirstNameIsNot: " + employeeRepository.findByFirstNameIsNot(""));
+        System.out.println("findByLastNameStartsWith: " + employeeRepository.findByLastNameStartsWith(""));
+        System.out.println("findBySalaryGreaterThan: " + employeeRepository.findBySalaryGreaterThan(0));
+        System.out.println("findBySalaryLessThan: " + employeeRepository.findBySalaryLessThan(0));
+        //System.out.println("findByHireDateBetween: " + employeeRepository.findByHireDateBetween());
+        System.out.println("findBySalaryGreaterThanEqualOrderBySalaryDesc: " + employeeRepository.findBySalaryGreaterThanEqualOrderBySalaryDesc(0));
+        System.out.println("findDistinctTop3BySalaryLessThan: " + employeeRepository.findDistinctTop3BySalaryLessThan(0));
+        System.out.println("findByEmailIsNull: " + employeeRepository.findByEmailIsNull());
+        System.out.println("-----------------Employee end----------------");
     }
 
 }
--- Spring-28-ORM-DerivedQueries/src/main/java/com/cyber/repository/EmployeeRepository.java
+++ Spring-28-ORM-DerivedQueries/src/main/java/com/cyber/repository/EmployeeRepository.java
@@ -1,9 +1,44 @@
 package com.cyber.repository;
 
 import com.cyber.entity.Employee;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+    //display all employees withe email address
+    List<Employee> findByEmail(String email);
+
+    //display all employees with first name '' and last name '', also show all employees with email ''
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstName,String lastName,String email);
+
+    //display all employees that first name is not ''
+    List<Employee> findByFirstNameIsNot(String firstName);
+
+    //display all employees where last name starts with ''
+    List<Employee> findByLastNameStartsWith(String pattern);
+
+    //display all employees with salary higher than ''
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    //display all employees with salary less than ''
+    List<Employee> findBySalaryLessThan(Integer salary);
+
+    //display all employees that has been hired between '' and ''
+    List<Employee> findByHireDateBetween(LocalDate startDate,LocalDate endDate);
+
+    //display all employees greater and equal to '' in order
+    List<Employee> findBySalaryGreaterThanEqualOrderBySalaryDesc(Integer salary);
+
+    //display top 3 unique employees that is making less than ''
+    List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
+
+    //display all employees that do not have email address
+    List<Employee> findByEmailIsNull();
+
 }
