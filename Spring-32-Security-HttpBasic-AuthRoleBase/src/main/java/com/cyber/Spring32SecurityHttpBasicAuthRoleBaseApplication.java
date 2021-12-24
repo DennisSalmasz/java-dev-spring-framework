4ee084278c31614f76a3e81ff5e3d838194a4eaa
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-24 19:47:47 +0000

    Security - HttpBasic - AuthRoleBase

---
+++ Spring-32-Security-HttpBasic-AuthRoleBase/src/main/java/com/cyber/Spring32SecurityHttpBasicAuthRoleBaseApplication.java
@@ -0,0 +1,13 @@
+package com.cyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring32SecurityHttpBasicAuthRoleBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring32SecurityHttpBasicAuthRoleBaseApplication.class, args);
+    }
+
+}
