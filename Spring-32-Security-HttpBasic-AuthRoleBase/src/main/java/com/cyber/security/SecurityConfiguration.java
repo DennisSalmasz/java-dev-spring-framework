00c259f934953d7dbc2e2b64d8ba5d83c30fef1c
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-24 21:46:01 +0000

    Security - HttpBasic - AuthRoleBase

--- Spring-32-Security-HttpBasic-AuthRoleBase/src/main/java/com/cyber/security/SecurityConfiguration.java
+++ Spring-32-Security-HttpBasic-AuthRoleBase/src/main/java/com/cyber/security/SecurityConfiguration.java
@@ -15,15 +15,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     //steps for custom security configuration:
     //1. extend WebSecurityConfigurerAdapter class
-    //2. override configure(HttpSecurity http) [for form structure] & configure(AuthenticationManagerBuilder auth) [for authentication]
+    //2. override configure(HttpSecurity http) [for form structure] &
+    //            -- we deal with authorization [antMatchers() identifies who will access what]
+    //            configure(AuthenticationManagerBuilder auth) [for authentication]
+    //            -- we describe roles in this method - ADMIN - MANAGER - USER
     //3. add @Configuration & @EnableWebSecurity annotations
     //4. create PasswordEncoder bean to encode passwords
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests() //request should be authorized
-                .anyRequest().authenticated() // incoming request be authenticated
+                .authorizeRequests() //every request should be authorized
+                .antMatchers("index.html").permitAll() //give access to everyone
+                .antMatchers("/profile/**").authenticated() //any role [admin, manager, user] can access profile folder
+                .antMatchers("/admin/**").hasRole("ADMIN") //only admin can access admin folder
+                .antMatchers("/management/**").hasAnyRole("ADMIN","MANAGER") //admin and manager can access management folder
                 .and()
                 .httpBasic(); //perform basic http authentication
     }
@@ -33,6 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER")
                 .and()
                 .withUser("dennis").password(passwordEncoder().encode("dennis123")).roles("USER");
     }
