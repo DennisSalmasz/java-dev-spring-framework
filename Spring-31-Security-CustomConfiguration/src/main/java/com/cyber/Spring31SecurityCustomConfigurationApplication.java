fbe93f4eb807f8bc40a0e0506e612acb6158577f
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-24 17:41:46 +0000

    Custom Configuration

---
+++ Spring-31-Security-CustomConfiguration/src/main/java/com/cyber/Spring31SecurityCustomConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.cyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring31SecurityCustomConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring31SecurityCustomConfigurationApplication.class, args);
+    }
+
+}
