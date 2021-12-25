3ae511f7ade7cc77d842b32b2b75899552c32ba9
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-25 16:16:27 +0000

    Security - HttpBasic - FormAuth

---
+++ Spring-34-Security-FormAuth/src/main/java/com/cyber/Spring34SecurityFormAuthApplication.java
@@ -0,0 +1,13 @@
+package com.cyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring34SecurityFormAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring34SecurityFormAuthApplication.class, args);
+    }
+
+}
