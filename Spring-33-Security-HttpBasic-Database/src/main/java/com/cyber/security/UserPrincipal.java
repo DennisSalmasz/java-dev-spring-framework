2e1065b5f088f4dab52ac8ed38c702f48270a716
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-25 03:45:45 +0000

    Security - HttpBasic - Database

---
+++ Spring-33-Security-HttpBasic-Database/src/main/java/com/cyber/security/UserPrincipal.java
@@ -0,0 +1,73 @@
+package com.cyber.security;
+
+import com.cyber.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+//UserDetail class is Spring's security authentication class
+//getAuthorities() inside that class contains user roles & permissions, based on naming convention
+
+public class UserPrincipal implements UserDetails {
+
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        //extract list of permissions
+        user.getPermissionList().forEach(p -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority(p);
+            authorityList.add(authority);
+        });
+
+        //extract list of roles
+        user.getRoleList().forEach(a -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + a); //naming convention !!
+            authorityList.add(authority);
+        });
+
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.getActive() == 1;
+    }
+
+}
