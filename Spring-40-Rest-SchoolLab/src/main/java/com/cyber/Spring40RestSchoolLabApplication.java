88fbbfc804e85772546aedfee527db7d5ddb2d98
DennisSalmaz <deniz.salmazs@gmail.com>
2022-01-02 01:26:30 +0000

    Spring REST - School Lab

--- Spring-40-Rest-SchoolLab/src/main/java/com/cyber/Spring40RestSchoolLabApplication.java
+++ Spring-40-Rest-SchoolLab/src/main/java/com/cyber/Spring40RestSchoolLabApplication.java
@@ -2,12 +2,19 @@ package com.cyber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring40RestSchoolLabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring40RestSchoolLabApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
--- Spring-40-Rest-SchoolLab/src/main/java/com/cyber/controller/ApiController.java
+++ Spring-40-Rest-SchoolLab/src/main/java/com/cyber/controller/ApiController.java
@@ -1,17 +1,19 @@
 package com.cyber.controller;
 
+import com.cyber.model.Address;
 import com.cyber.model.ResponseWrapper;
 import com.cyber.model.Teacher;
+import com.cyber.repository.AddressRepository;
 import com.cyber.repository.ParentRepository;
 import com.cyber.repository.StudentRepository;
 import com.cyber.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApiController {
@@ -21,7 +23,9 @@ public class ApiController {
     @Autowired
     private StudentRepository studentRepository;
     @Autowired
-    ParentRepository parentRepository;
+    private ParentRepository parentRepository;
+    @Autowired
+    private AddressRepository addressRepository;
 
     @GetMapping("/teachers")
     public List<Teacher> readAllTeachers(){
@@ -41,5 +45,16 @@ public class ApiController {
                                                                      HttpStatus.ACCEPTED.value(),
                                                                      parentRepository.findAll());
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseWrapper);
+    }
+
+    @PutMapping("/address/{id}")
+    public Address updateAddress(@PathVariable("id") Long id, @RequestBody Address address) throws Exception {
+        Optional<Address> foundAddress = addressRepository.findById(id);
+        if(!foundAddress.isPresent()){
+            throw new Exception("Address does not exist !!");
+        }
+        address.setCurrentTemperature(new Address().consumeTemp(address.getCity()));
+        address.setId(foundAddress.get().getId());
+        return addressRepository.save(address);
     }
 }
