6b4f2e4eff678bfd9b83103fc387f371a8565cf9
DennisSalmaz <deniz.salmazs@gmail.com>
2022-01-03 00:56:39 +0000

    Spring REST - WebClient

--- Spring-38-Rest-Jackson/src/main/java/com/cyber/repository/GenreRepository.java
+++ Spring-38-Rest-Jackson/src/main/java/com/cyber/repository/GenreRepository.java
@@ -2,8 +2,12 @@ package com.cyber.repository;
 
 import com.cyber.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre,Long> {
+
+    @Query(value = "SELECT count(*) from genre g JOIN movie_genre_rel mgr on g.id = mgr.genre_id WHERE g.id = ?1", nativeQuery = true)
+    Integer countGenresNativeQuery(Long id);
 }
