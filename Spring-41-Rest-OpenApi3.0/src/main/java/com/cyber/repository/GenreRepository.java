1aeda6245dc02960562e3ea137337e7ea0020eb3
DennisSalmaz <deniz.salmazs@gmail.com>
2022-01-04 13:12:05 +0000

    Spring REST - OpenApi3.0

--- Spring-41-Rest-OpenApi3.0/src/main/java/com/cyber/repository/GenreRepository.java
+++ Spring-41-Rest-OpenApi3.0/src/main/java/com/cyber/repository/GenreRepository.java
@@ -9,15 +9,4 @@ import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre,Long> {
-    // ------------------- JPQL QUERIES ------------------- //
-//Write a JPQL query that return all genres
-    @Query("SELECT g FROM Genre g")
-    List<Genre> returnAll();
-
-    // ------------------- Native QUERIES ------------------- //
-//Write a native query that returns genres by containing name
-    @Query(value = "SELECT * FROM genre WHERE name LIKE concat('%',?1,'%')",
-            nativeQuery = true)
-    List<Genre> returnGenreBaseOnName(String name);
-
 }
