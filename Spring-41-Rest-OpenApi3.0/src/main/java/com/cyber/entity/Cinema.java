70725e5df1156b8b00a1e4fdbcbd456364e95283
DennisSalmaz <deniz.salmazs@gmail.com>
2022-01-04 02:38:01 +0000

    Spring REST - OpenApi3.0

---
+++ Spring-41-Rest-OpenApi3.0/src/main/java/com/cyber/entity/Cinema.java
@@ -0,0 +1,26 @@
+package com.cyber.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Cinema extends BaseEntity {
+
+    private String name;
+    private String sponsoredName;
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    public Cinema(String name, String sponsoredName) {
+        this.name = name;
+        this.sponsoredName = sponsoredName;
+    }
+}
