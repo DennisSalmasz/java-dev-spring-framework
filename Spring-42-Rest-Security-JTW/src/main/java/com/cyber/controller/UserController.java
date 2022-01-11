f53acfc55712371f2c5be89ba2c6e828daaf75c8
DennisSalmaz <deniz.salmazs@gmail.com>
2022-01-11 23:17:07 +0000

    Spring Security - JWT: token

--- Spring-42-Rest-Security-JTW/src/main/java/com/cyber/controller/UserController.java
+++ Spring-42-Rest-Security-JTW/src/main/java/com/cyber/controller/UserController.java
@@ -5,6 +5,8 @@ import com.cyber.entity.User;
 import com.cyber.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@EnableGlobalMethodSecurity(prePostEnabled = true) //this is for authorization
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/read")
+    //@PreAuthorize("hasAuthority('USER')") //only admin is authorized for this endpoint !!
+    @PreAuthorize("hasAnyAuthority('USER','ADMIN')") //user & admin have access to this endpoint
     public ResponseEntity<ResponseWrapper> readAll(){
         List<User> users = userService.getAll();
         return ResponseEntity.ok(new ResponseWrapper("Done",users));
