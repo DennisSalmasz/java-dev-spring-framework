15e7264757122483b6b2004335fa33783e88da50
DennisSalmaz <deniz.salmazs@gmail.com>
2022-01-16 16:02:26 +0000

    Spring AOP

---
+++ Spring-43-AOP/src/main/java/com/cyber/Spring43AopApplication.java
@@ -0,0 +1,13 @@
+package com.cyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring43AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring43AopApplication.class, args);
+    }
+
+}
---
+++ Spring-43-AOP/src/main/java/com/cyber/implementation/ProductServiceImpl.java
@@ -0,0 +1,49 @@
+package com.cyber.implementation;
+
+import com.cyber.entity.Product;
+import com.cyber.repository.ProductRepository;
+import com.cyber.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> delete(Long id) {
+        productRepository.deleteById(id);
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> updateProduct(Long id, Product product) {
+
+        Product obj = productRepository.findById(id).get();
+        obj.setName(product.getName());
+        productRepository.save(obj);
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> createProduct(Product product) {
+        productRepository.save(product);
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProduct(Long id) {
+        return productRepository.findById(id).get();
+    }
+}
