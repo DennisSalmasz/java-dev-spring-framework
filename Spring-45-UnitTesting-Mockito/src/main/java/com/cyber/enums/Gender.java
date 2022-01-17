9b30f922931503903e9836b764d4486fb2f68737
DennisSalmaz <deniz.salmazs@gmail.com>
2022-01-17 22:10:51 +0000

    Unit Test - Mockito

---
+++ Spring-45-UnitTesting-Mockito/src/main/java/com/cyber/enums/Gender.java
@@ -0,0 +1,16 @@
+package com.cyber.enums;
+
+public enum Gender {
+    
+    MALE("Male"), FEMALE("Female");
+    
+    private final String value;
+
+    private Gender(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
